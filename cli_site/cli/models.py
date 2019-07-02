0f3b88b3ee8fc8b4b7e7456111043785584e51be
OneTallProgrammer <jmcauthen@live.com>
2019-07-02 13:04:46 -0700

    added path('', include('app.urls')) to cli_site/urls.py added urlpattern path('', views.cli, name='cli') to cli/urls.py added cli function to cli/views.py

--- cli_site/cli/models.py
+++ cli_site/cli/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
+class Command(models.Model):
+    cmd = models.CharField(max_length=200)
+    timestamp = models.DateTimeField()
+
+    def write(self):
+        self.timestamp = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.cmd
--- cli_site/cli/views.py
+++ cli_site/cli/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def cli(request):
+    return render(request, 'cli/index.html', {})
